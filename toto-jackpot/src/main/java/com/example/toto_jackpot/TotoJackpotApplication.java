a63bc0de77fb89c8cfc6fe93714126112c5b4617
Shawn Yip-Au <shawnyip-au@users.noreply.github.com>
2025-10-24 15:09:28 +0800

    Create producer

---
+++ toto-jackpot/src/main/java/com/example/toto_jackpot/TotoJackpotApplication.java
@@ -0,0 +1,13 @@
+package com.example.toto_jackpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotoJackpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotoJackpotApplication.class, args);
+	}
+
+}
---
+++ toto-jackpot/src/test/java/com/example/toto_jackpot/TotoJackpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toto_jackpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotoJackpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

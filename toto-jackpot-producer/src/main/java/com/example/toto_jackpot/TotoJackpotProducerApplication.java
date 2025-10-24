b4af76dfd8dc26a6fbecf36dd5886fdb1c033f13
Shawn Yip-Au <shawnyip-au@users.noreply.github.com>
2025-10-24 17:50:50 +0800

    Rename project

--- toto-jackpot-producer/src/main/java/com/example/toto_jackpot/TotoJackpotProducerApplication.java
+++ toto-jackpot-producer/src/main/java/com/example/toto_jackpot/TotoJackpotProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TotoJackpotApplication {
+public class TotoJackpotProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TotoJackpotApplication.class, args);
+		SpringApplication.run(TotoJackpotProducerApplication.class, args);
 	}
 
 }

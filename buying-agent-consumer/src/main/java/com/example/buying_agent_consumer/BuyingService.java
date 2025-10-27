4d8cd805c2ea57a0b9adcde5e3b793790eee4b8f
Shawn Yip-Au <shawnyip-au@users.noreply.github.com>
2025-10-27 12:31:05 +0800

    Use ConsumerRecord instead of raw strings

--- buying-agent-consumer/src/main/java/com/example/buying_agent_consumer/BuyingService.java
+++ buying-agent-consumer/src/main/java/com/example/buying_agent_consumer/BuyingService.java
@@ -1,5 +1,8 @@
 package com.example.buying_agent_consumer;
 
+//https://kafka.apache.org/21/javadoc/org/apache/kafka/clients/consumer/ConsumerRecord.html
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +10,20 @@ import org.springframework.stereotype.Service;
 public class BuyingService {
     private static final String TOPIC = "jackpot-topic";
 
-    @KafkaListener(topics = TOPIC, groupId = TOPIC)
-    public void consumeNewJackpot(String jackpot) {
-        System.out.println("New jackpot event: " + jackpot);
+    @KafkaListener(topics = TOPIC, groupId = "${spring.application.name}")
+     public void consumeNewJackpot(ConsumerRecord<String, String> record) {
+        System.out.println(
+            "key: " + record.key() + "\n" +
+            "value: " + record.value() + "\n" +
+            "topic: " + record.topic() + "\n" +
+            "partition: " + record.partition() + "\n" +
+            "offset: " + record.offset() + "\n" +
+            "timestamp: " + record.timestamp() + "\n" +
+            "timestampType: " + record.timestampType() + "\n" +
+            "headers: " + record.headers() + "\n" +
+            "serializedKeySize: " + record.serializedKeySize() + "\n" +
+            "serializedValueSize: " + record.serializedValueSize() + "\n" +
+            "leaderEpoch: " + record.leaderEpoch().orElse(null) + "\n"
+        );
     }
 }
